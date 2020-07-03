227901cd9ae17af953f24e43441ef2aeec695c50
Jonathan Perry <jpper@umich.edu>
2020-07-03 22:37:16 +0900

    Changed name of app in package.json from devdactic and also localized app for Japan

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,17 +1,17 @@
-import { NgModule, LOCALE_ID } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
-import { HomePage } from './home.page';
+import { NgModule, LOCALE_ID } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { IonicModule } from "@ionic/angular";
+import { FormsModule } from "@angular/forms";
+import { HomePage } from "./home.page";
 
-import { HomePageRoutingModule } from './home-routing.module';
+import { HomePageRoutingModule } from "./home-routing.module";
 
-import { NgCalendarModule } from 'ionic2-calendar';
-import { CalModalPageModule } from '../cal-modal/cal-modal.module';
+import { NgCalendarModule } from "ionic2-calendar";
+import { CalModalPageModule } from "../cal-modal/cal-modal.module";
 
-import { registerLocaleData } from '@angular/common';
-import localeDe from '@angular/common/locales/de';
-registerLocaleData(localeDe);
+import { registerLocaleData } from "@angular/common";
+import localeJa from "@angular/common/locales/ja";
+registerLocaleData(localeJa);
 @NgModule({
   imports: [
     CommonModule,
@@ -19,9 +19,9 @@ registerLocaleData(localeDe);
     IonicModule,
     HomePageRoutingModule,
     NgCalendarModule,
-    CalModalPageModule
+    CalModalPageModule,
   ],
   declarations: [HomePage],
-  providers: [{ provide: LOCALE_ID, useValue: 'de-DE' }]
+  providers: [{ provide: LOCALE_ID, useValue: "ja-JA" }],
 })
 export class HomePageModule {}
